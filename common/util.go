ff1157c42998ed718f0627a8417632ebb25e424c
stephenjohnkomna <stephenjohn.tech@gmail.com>
2022-05-30 02:18:54 +0100

    created a Look up function to get corresponding value for the letter supplied

--- common/util.go
+++ common/util.go
@@ -47,6 +47,13 @@ func FindCountryByCode(CountryList models.CountryList, countryCode string) model
 	return country
 }
 
-func MoveInitialCharacterBehind(iban string) {
-
+func LookUpTable(letter string) int {
+	m := map[string]int{
+		"A": 10, "B": 11, "C": 12, "D": 13, "E": 14, "F": 15,
+		"G": 16, "H": 17, "I": 18, "J": 19, "K": 20, "L": 21,
+		"M": 22, "N": 23, "O": 24, "P": 25, "Q": 26, "R": 27,
+		"S": 28, "T": 29, "U": 30, "V": 31, "W": 32, "X": 33,
+		"Y": 34, "Z": 35,
+	}
+	return m[letter]
 }
