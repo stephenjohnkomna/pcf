9251e85e5cb5d972b2d503f5bc8fff4b39022482
stephenjohnkomna <stephenjohn.tech@gmail.com>
2022-05-30 02:53:24 +0100

    Created Validate Country Code Function

--- common/util.go
+++ common/util.go
@@ -9,12 +9,7 @@ import (
 	"strconv"
 )
 
-func ValidateIban(iban string) dto.IbanResponse {
-	var ibanResponse dto.IbanResponse
-	return ibanResponse
-}
-
-func ConvertJsonToStruct(filename string) (models.CountryList, error) {
+func GetListOfCountries(filename string) (models.CountryList, error) {
 	// Open the jsonFile
 	jsonfile, err := ioutil.ReadFile(filename)
 
@@ -48,6 +43,7 @@ func FindCountryByCode(CountryList models.CountryList, countryCode string) model
 	return country
 }
 
+// Function to Look Letter for its corresponding value representation
 func LookUpTable(letter string) int {
 	m := map[string]int{
 		"A": 10, "B": 11, "C": 12, "D": 13, "E": 14, "F": 15,
@@ -75,4 +71,45 @@ func ConvertToInteger(rearrangeIban string) string {
 
 	}
 	return rerrangedIbanToIntegerValue
+}
+
+// Validate that the Country Code is Valid
+func ValidateCountryCode(code string) bool {
+	countries, _ := GetListOfCountries("country.json")
+	country := FindCountryByCode(countries, code)
+	if country.Code == code {
+		return true
+	} else {
+		return false
+	}
+}
+
+func ValidateIban(iban string) dto.IbanResponse {
+	// Initialize IbanResponse
+	var ibanResponse dto.IbanResponse
+	ibanResponse.IsValid = false
+	ibanResponse.Message = "IBAN is NOT VALID"
+
+	// • IBAN:		GB82 WEST 1234 5698 7654 32
+	// Validate Country Code
+	countryCode := iban[0:2]
+	if ValidateCountryCode(countryCode) != true {
+		return ibanResponse
+	}
+
+	// • Rearrange:		W E S T12345698765432 G B82
+	// First 2 characters are Country Code and the Next 2 characters are Check digit to be moved to the end of the IBAN
+	rearrangedIban := iban[4:len(iban)] + iban[0:4]
+
+	// • Convert to integer:		3214282912345698765432161182
+	integerIbanConversion := ConvertToInteger(rearrangedIban)
+
+	// • Compute remainder:		3214282912345698765432161182	mod 97 = 1
+	isValid := strconv.ParseInt(integerIbanConversion, 10, 64) % 97
+
+	if isValid == true {
+		ibanResponse.IsValid = true
+		ibanResponse.Message = "IBAN is VALID"
+	}
+	return ibanResponse
 }
